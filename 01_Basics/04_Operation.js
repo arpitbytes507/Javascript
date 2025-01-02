0b98e412298263c4ec71ec08f9543807ece7775a
arpitbytes507 <arpit.dhumane@gmail.com>
2025-01-02 14:03:09 +0000

    Operators done

---
+++ 01_Basics/04_Operation.js
@@ -0,0 +1,12 @@
+let str = "Arpit"
+let str1 = " Dhumane"
+console.log(str + str1)
+console.log(2 + 1 + "3")
+console.log("2" + 1 + 3)
+
+// Operations in Javascript
+// 1.Arithmetic Operators(+,-,*,%,**,/)
+// 2.Comparison Operators(==,===(it check datatype also)etc)
+// 3.Logical Operators(&&,||,!)
+// 4.Assignment Operators(=,+=,-=)
+// 5.Bitwise Operators(&,|,~)
