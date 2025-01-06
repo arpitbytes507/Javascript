7ea9eaffb91af2a9159cd4e533addc775b839e27
arpitbytes507 <arpit.dhumane@gmail.com>
2025-01-06 16:46:07 +0000

    Math

---
+++ 03_Number_Math/02_Math.js
@@ -0,0 +1,17 @@
+// The JS Math object allows you to perform mathematical tasks on numbers
+// Math Methods
+// Math.round() returns the nearest integer
+console.log(Math.round(4.6))
+console.log(Math.round(2.2))
+// Math.ceil() returns the value of rounded up to its bigesst nearest integer
+console.log(Math.ceil(2.1))
+console.log(Math.ceil(4.9))
+// Math.floor() is exactly opposite of the Math.ceil()
+// Math.pow(x,y) returns the value of x to power y
+console.log(Math.pow(8, 2))
+// Math.sign() returns the whether integer is positive , null or negative
+console.log(Math.sign(-4))
+// Math.sqrt() gives the square root of the enter number
+// Math.abs() gives the absolute value of the given number
+console.log(Math.abs(4.80))
+// Math.random() returns a random number between 0, and 1 
