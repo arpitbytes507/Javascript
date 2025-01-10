bab36aca480645601f07f26a5dc6e861fee57474
arpitbytes507 <arpit.dhumane@gmail.com>
2025-01-10 16:45:59 +0000

    for in loop

--- 09_Iterations/02_For_in.js
+++ 09_Iterations/02_For_in.js
@@ -2,6 +2,9 @@ const num = [1,2,3,4,5]     // it prints index as key value for array is index
 for (const key in num) {
     console.log(key)
 }
+for (const key in num) {
+    console.log(num[key])
+}
 const heroes = {               // for in can iterates the object keys but not maps as not iterables
     "Avengers" :"Ironman",
     "DC" : "Superman",
